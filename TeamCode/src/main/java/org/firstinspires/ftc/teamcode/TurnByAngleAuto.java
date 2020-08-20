616e213ed6e927ff1ca68c2cba6208f851b69ca9
roboken-dev <ckunkel@hobokencs.org>
2020-08-20 10:03:46 -0400

    Commented turn by angle java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurnByAngleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurnByAngleAuto.java
@@ -1,18 +1,4 @@
-/*
-To add this code to your folder, right click on org.firstinspires.ftc.teamcode on the left Project navigation.
-Select New -> Java Class
-Make sure you name the class TeleOpTutorial
-Copy and paste everything from this doc into the Class replacing what is there with this code.
-This should work.
 
-This code was created by following the tutorial created by Swerve Robotics found on YouTube:
-https://www.youtube.com/watch?v=OT_PGYIFBGE&list=PLJIJCo7cYsE-ma0iYtbCf27s7zgLq-73i&index=8
-
-Note: Since the Jersey Bots do not have servos, that part of the code was omitted.
-
-IT IS STRONGLY RECOMMENDED YOU TAKE THE TIME TO DO THE TUTORIAL YOURSELF.
-IT DOESN'T TAKE LONG AND WILL COVER A FEW IMPORTANT BASIC CONCEPTS.
-*/
 
 
 package org.firstinspires.ftc.teamcode;
@@ -31,7 +17,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 @Autonomous(name="TurnByAngle")
 
 
-public class TurnByAngleTest extends LinearOpMode
+public class TurnByAngleAuto extends LinearOpMode
 {
     private DcMotor motorLeft;
     private DcMotor motorRight;
@@ -44,6 +30,8 @@ public class TurnByAngleTest extends LinearOpMode
 
     public void runOpMode()
     {
+
+        //Initialize the IMU and its parameters.
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
         parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
@@ -52,12 +40,19 @@ public class TurnByAngleTest extends LinearOpMode
         parameters.loggingTag          = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
-        motorLeft = hardwareMap.dcMotor.get("motorLeft");
-        motorRight = hardwareMap.dcMotor.get("motorRight");
-
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
 
+        //Hardware map
+        motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        motorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //The IMU does not initialize instantly. This makes it so the driver can see when they can push Play without errors.
         telemetry.addData("Mode", "calibrating...");
         telemetry.update();
         while (!isStopRequested() && !imu.isGyroCalibrated())
@@ -66,35 +61,34 @@ public class TurnByAngleTest extends LinearOpMode
             idle();
         }
 
+        //Tells the driver it is ok to start.
         telemetry.addData("Mode", "waiting for start");
         telemetry.addData("imu calib status", imu.getCalibrationStatus().toString());
         telemetry.update();
 
-        motorLeft.setDirection(DcMotor.Direction.REVERSE);
-
-        motorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        //variable for how fast the robot willmove
         double DRIVE_POWER = 0.5;
 
 
         waitForStart();
 
+
+        //displays "running" when
         telemetry.addData("Mode", "running");
         telemetry.update();
 
 
+        //Actual series of motions the robot does
         DriveForwardByTime(DRIVE_POWER,1000);
         rotate(90, 0.3);
         DriveForwardByTime(DRIVE_POWER,1000);
         rotate(-90, 0.3);
 
-
-
-
     }
 
 
+
+    //Method driving forward by time
     public void DriveForwardByTime(double power, long time)
     {
     motorLeft.setPower(power);
@@ -107,6 +101,7 @@ public class TurnByAngleTest extends LinearOpMode
     }
 
 
+    //Method for turning right, by TIME, not IMU
     public void TurnRightByTime(double power, long time)
     {
     motorLeft.setPower(power);
@@ -118,7 +113,7 @@ public class TurnByAngleTest extends LinearOpMode
     sleep(250);
     }
 
-
+    //Method for turning left, by TIME, not IMU
     public void TurnLeftByTime(double power, long time)
     {
         motorLeft.setPower(-power);
@@ -131,6 +126,7 @@ public class TurnByAngleTest extends LinearOpMode
     }
 
 
+    //This method reads the IMU getting the angle. It automatically adjusts the angle so that it is between -180 and +180.
     public double getAngle()
     {
         Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
@@ -152,18 +148,20 @@ public class TurnByAngleTest extends LinearOpMode
 
 
 
+    //The method turns the robot by a specific angle, -180 to +180.
     public void rotate(int degrees, double power)
     {
         double  leftPower, rightPower;
 
         resetAngle();
 
+        //if the degrees are less than 0, the robot will turn right
         if (degrees < 0)
         {
             leftPower = power;
             rightPower = -power;
         }
-        else if (degrees > 0)
+        else if (degrees > 0)//if greater than 0, turn left
         {
             leftPower = -power;
             rightPower = power;
@@ -171,9 +169,11 @@ public class TurnByAngleTest extends LinearOpMode
         else return;
 
 
+        //sets power to motors with negative signs properly assigned to make the robot go in the correct direction
         motorLeft.setPower(leftPower);
         motorRight.setPower(rightPower);
 
+        //Repeatedly check the IMU until the getAngle() function returns the value specified.
         if (degrees < 0)
         {
             while (opModeIsActive() && getAngle() == 0) {}
@@ -184,10 +184,12 @@ public class TurnByAngleTest extends LinearOpMode
             while (opModeIsActive() && getAngle() < degrees) {}
 
 
+        //stop the motors after the angle has been found.
 
         motorLeft.setPower(0);
         motorRight.setPower(0);
 
+        //sleep for a bit to make sure the robot doesn't over shoot
         sleep(1000);
 
         resetAngle();
@@ -196,6 +198,7 @@ public class TurnByAngleTest extends LinearOpMode
 
 
 
+    //this method resets the angle so that the robot's heading is now 0
     public void resetAngle()
     {
         lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
